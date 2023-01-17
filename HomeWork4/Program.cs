1de8201467c3ec05291db0080ed6324135008659
Rishat_Imangulov <gr40in@yandex.ru>
2023-01-18 03:12:47 +0500

    done HW4

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+
+if (x > 0)  // двигаемся вправо от 1 до нужного числа 
+{
+    int count = 2;
+    while (count <= x)
+    {
+        Console.Write($"{count} ");
+        count += 2;
+    }
+}
+else // x<=0:
+{
+    int count = 0; // 0 - четное число
+    while (count >= x)
+    {
+        Console.Write($"{count} ");
+        count -= 2;
+    }
+}
+
