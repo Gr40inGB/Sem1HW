51e65367affcd5a0735a110985df3e09fc1af7fc
Rishat_Imangulov <gr40in@yandex.ru>
2023-01-18 02:45:00 +0500

    done HW2

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -9,8 +9,28 @@
 
 Console.Clear();
 
-Console.Write("Введите первое число: ");
-float x1 = float.Parse(Console.ReadLine());
+float x1 = 0; float x2 = 0; float x3 = 0;
+bool error = true;
 
-Console.Write("Введите второе число: ");
-float x2 = float.Parse(Console.ReadLine());
+while (error)
+{
+    Console.Write("Введите первое число: "); x1 = float.Parse(Console.ReadLine());
+    Console.Write("Введите второе число: "); x2 = float.Parse(Console.ReadLine());
+    if (x1 == x2)
+    {
+        error = true; Console.WriteLine("Необходимо вводить разные числа"); continue;
+    }
+    Console.Write("Введите третье число: "); x3 = float.Parse(Console.ReadLine());
+    if (x1 == x3 || x2 == x3)
+    {
+        error = true; continue; Console.WriteLine("Необходимо вводить разные числа"); continue;
+    }
+    else error = false;
+}
+
+float max = x1;          // max = x1
+
+if (max < x2) max = x2;  // else x1 > x2  max = x1 
+if (max < x3) max = x3;  // else x3 < max  max = x1 or x2
+
+Console.Write($" Максимальное число = {max} ");
